25a5e34cd4252e06ea9e3bb5dff5a6570e3ab637
hyungyoo <hjyoo901112@gmail.com>
2023-04-03 22:27:24 +0200

    fix: Install asaa for gulp

--- part_1/Gulpfile.js
+++ part_1/Gulpfile.js
@@ -2,18 +2,18 @@
 const gulp = require("gulp");
 
 // Sass 컴파일러 모듈 로드
-const sass = require("gulp-sass");
+const sass = require("gulp-sass")(require("sass"));
 
 gulp.task("sass", function () {
   return gulp
-    .src("scss/*.scss")
+    .src("./scss/*.scss")
     .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest("dist/css"));
+    .pipe(gulp.dest("./dist/css"));
 });
 
 // 파일 변경 감지 및 자동 변환
 gulp.task("watch", function () {
-  gulp.watch("scss/*.scss", gulp.series("sass"));
+  gulp.watch("./scss/*.scss", gulp.series("sass"));
 });
 
 // 기본 작업 설정
