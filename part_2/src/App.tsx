edd47b2c25c60b63f3171e14c2113cdc0bbea204
hyungyoo <hjyoo901112@gmail.com>
2023-04-04 18:39:52 +0200

    feat: Add styles SCSS

--- part_2/src/App.tsx
+++ part_2/src/App.tsx
@@ -1,40 +1,39 @@
-import React, { useCallback, useState } from "react";
-import "./App.css";
-import useAccessToken from "./hooks/useAccessToken";
-import axios from "axios";
+import "./styles/reset.scss";
 
 function App() {
-  const accessToken = useAccessToken();
-  const [search, setSearch] = useState("");
-
-  const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(e?.target.value);
-  }, []);
-
-  // console.log(search);
-  console.log(accessToken);
-
-  const onClick = useCallback(
-    (e: React.MouseEvent<HTMLButtonElement>) => {
-      axios
-        .get("https://api.spotify.com/v1/browse/featured-playlists", {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${accessToken}`,
-          },
-        })
-        .then((res) => console.log(res.data));
-    },
-    [search, accessToken]
-  );
-
-  return (
-    <div>
-      <p>hello </p>
-      <input onChange={onChange} />
-      <button onClick={onClick} />
-    </div>
-  );
+  return <div>hello</div>;
 }
 
 export default App;
+
+// const accessToken = useAccessToken();
+// const [search, setSearch] = useState("");
+
+// const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+//   setSearch(e?.target.value);
+// }, []);
+
+// // console.log(search);
+// console.log(accessToken);
+
+// const onClick = useCallback(
+//   (e: React.MouseEvent<HTMLButtonElement>) => {
+//     axios
+//       .get("https://api.spotify.com/v1/browse/featured-playlists", {
+//         headers: {
+//           "Content-Type": "application/json",
+//           Authorization: `Bearer ${accessToken}`,
+//         },
+//       })
+//       .then((res) => console.log(res.data));
+//   },
+//   [search, accessToken]
+// );
+
+// return (
+//   <div>
+//     <p>hello </p>
+//     <input onChange={onChange} />
+//     <button onClick={onClick} />
+//   </div>
+// );
